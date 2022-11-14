de10c7114d070a3243fa0c69a4c12de7a54cbb32
eartser <eartser19@gmail.com>
2022-11-14 23:28:31 +0300

    add characteristics

--- roguelike/src/main/kotlin/entity/models/EntityWithCharacteristics.kt
+++ roguelike/src/main/kotlin/entity/models/EntityWithCharacteristics.kt
@@ -7,7 +7,15 @@ abstract class EntityWithCharacteristics(
     gameObject: GameObject,
     val characteristics: Characteristics
 ) : Entity(gameObject) {
-    fun reduceHealth(damage: Int) {
-        TODO()
-    }
+    fun increaseHealth(shift: Double) = characteristics.shiftHealth(shift)
+
+    fun decreaseHealth(shift: Double) = characteristics.shiftHealth(-shift)
+
+    fun increaseAttack(shift: Double) = characteristics.shiftAttack(shift)
+
+    fun decreaseAttack(shift: Double) = characteristics.shiftAttack(-shift)
+
+    fun increaseProtection(shift: Double) = characteristics.shiftProtection(shift)
+
+    fun decreaseProtection(shift: Double) = characteristics.shiftProtection(-shift)
 }
