bbb6ea5110583c3af2a5841781007e3cbdba63d1
eartser <eartser19@gmail.com>
2022-11-14 22:22:04 +0300

    update recipe item

--- roguelike/src/main/kotlin/inventory/grimoire/ItemRecipe.kt
+++ roguelike/src/main/kotlin/inventory/grimoire/ItemRecipe.kt
@@ -1,9 +1,16 @@
 package inventory.grimoire
 
+import inventory.items.EquipmentType
 import inventory.items.Item
 
 
-data class ItemRecipe(val ingredients: Map<Item, Int>, val resultingItem: Item) {
+data class ItemRecipe(
+    val ingredients: Map<Item, Int>,
+    val resultingItem: Item,
+    override val description: String
+): Item {
+    override val equipmentType: EquipmentType = EquipmentType.None
+
     fun isAppliable(availableItemsAmount: Map<Item, Int>): Boolean = ingredients.all { (item, count) ->
         availableItemsAmount.getOrDefault(item, 0) >= count
     }
